926b6f51dc3a9cb6a06617e1d7bc1772b65ef3aa
JoaoGuiBC <joaoguibc@gmail.com>
2022-02-11 23:15:30 -0300

    Setted basic style

---
+++ web/src/styles/theme.ts
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    background: '#F7F7F9',
+
+    text: '#0A0A0A',
+    subject: '#636363',
+
+    logo: '#3145AC',
+
+    main: '##35C388',
+    main_soft: '#ECFFF7',
+    attention: '#F34B60',
+    attention_soft: '#FFECEE',
+
+    shape: '#FFFFFF',
+  },
+};
