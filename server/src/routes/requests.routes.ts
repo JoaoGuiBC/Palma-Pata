af87ef3d9627cea36680feb9ac03777d12cacd61
JoaoGuiBC <joaoguibc@gmail.com>
2022-02-11 22:00:36 -0300

    Added the possibility to list the requests

--- server/src/routes/requests.routes.ts
+++ server/src/routes/requests.routes.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 
+import { prisma } from "../database/prismaClient";
 import { CreateRequestService } from "../services/CreateRequestService";
 
 const requestsRouter = Router();
@@ -8,6 +9,22 @@ interface IRequest {
   id_user: string;
   quantity: number;
 }
+
+requestsRouter.get("/listAll", async (_: Request, response: Response) => {
+  const requests = await prisma.requests.findMany();
+
+  response.json(requests);
+});
+
+requestsRouter.get("/", async (_: Request, response: Response) => {
+  const requests = await prisma.requests.findMany({
+    where: {
+      collected: false,
+    },
+  });
+
+  response.json(requests);
+});
 
 requestsRouter.post("/", async (request: Request, response: Response) => {
   const { id_user, quantity }: IRequest = request.body;
