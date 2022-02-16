a9e55728391e8e790d14a87507e58c1d50c3b43f
JoaoGuiBC <joaoguibc@gmail.com>
2022-02-16 17:54:16 -0300

    Restored AppError

--- server/src/errors/AppError.ts
+++ server/src/errors/AppError.ts
@@ -1,11 +1,12 @@
-export class AppError extends Error {
-  statusCode: number;
+class AppError {
+  public readonly message: string;
+
+  public readonly statusCode: number;
 
   constructor(message: string, statusCode = 400) {
-    super(message);
-    Error.captureStackTrace(this, this.constructor);
-
-    this.name = this.constructor.name;
+    this.message = message;
     this.statusCode = statusCode;
   }
 }
+
+export { AppError };
